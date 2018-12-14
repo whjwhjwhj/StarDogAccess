712a80e8126359f19fcba9b94dee565ec0e80bdb
whjwhjwhj <weihjemail@gmail.com>
2018-12-14 17:11:25 +0800

    增加读取StarDog --2018/12/14 增加了写入的内容。

--- App_StarDogAccess/views.py
+++ App_StarDogAccess/views.py
@@ -1,8 +1,34 @@
 from django.http import HttpResponse
 from SPARQLWrapper import SPARQLWrapper, POST, DIGEST
+from rdflib.plugins.stores import sparqlstore
+from rdflib import Graph, Literal, URIRef
+import rdflib
 
 # Create your views here.
 def index(request):
+
+    """write data into sht stardog"""
+    # Define the Stardog store
+    endpoint = 'http://123.206.66.105:5820/myDB/query'
+    store = sparqlstore.SPARQLUpdateStore()
+    store.open((endpoint, endpoint))
+    store.setCredentials('admin', 'admin')
+
+    # Identify a named graph where we will be adding our instances.
+    default_graph = URIRef('http://example.org/default-graph')
+    ng = Graph(store, identifier=default_graph)
+
+    g = Graph()
+    g.parse('./sample-concepts.ttl', format='turtle')
+    # Serialize our named graph to make sure we got what we expect.
+    print(g.serialize(format='turtle'))
+
+    ng.update(
+        u'INSERT DATA { %s }' % g.serialize(format='nt')
+    )
+
+
+    """read data from stardog"""
 
     queryString = "SELECT * WHERE { ?s ?p \"阿胶汤2\". }"
     sp = SPARQLWrapper("http://123.206.66.105:5820/myDB/query")
@@ -19,5 +45,7 @@ def index(request):
     ret = sp.query()
     for v in ret:
         print(v)
+
+
 
     return HttpResponse("Hello, World!  You're at the polls index.")
