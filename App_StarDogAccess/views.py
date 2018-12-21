fc9202ad42e2299bdd08afb3e61f6ed32001d83c
whjwhjwhj <weihjemail@gmail.com>
2018-12-21 17:56:29 +0800

    2018/12/21 同步

--- App_StarDogAccess/views.py
+++ App_StarDogAccess/views.py
@@ -1,51 +1,19 @@
-from django.http import HttpResponse
 from SPARQLWrapper import SPARQLWrapper, POST, DIGEST
 from rdflib.plugins.stores import sparqlstore
 from rdflib import Graph, Literal, URIRef
+from django.http import HttpResponse
 import rdflib
 
-# Create your views here.
-def index(request):
+queryString = "INSERT DATA { <http://example/book1>  <http://purl.org/dc/elements/1.1/subject>  \"WeiHaiJunbook\". }"
+sp = SPARQLWrapper("http://123.206.66.105:5820/TestDB/query")
 
-    """write data into sht stardog"""
-    # Define the Stardog store
-    endpoint = 'http://123.206.66.105:5820/myDB/query'
-    store = sparqlstore.SPARQLUpdateStore()
-    store.open((endpoint, endpoint))
-    store.setCredentials('admin', 'admin')
+#sp.setHTTPAuth(DIGEST)
+sp.setCredentials("admin", "admin")
+sp.setMethod(POST)
 
-    # Identify a named graph where we will be adding our instances.
-    default_graph = URIRef('http://example.org/default-graph')
-    ng = Graph(store, identifier=default_graph)
+# add a default graph, though that can also be part of the query string
+#sp.addDefaultGraph("http://localhost:5820/myDB/graph-selected")
 
-    g = Graph()
-    g.parse('./sample-concepts.ttl', format='turtle')
-    # Serialize our named graph to make sure we got what we expect.
-    print(g.serialize(format='turtle'))
-
-    ng.update(
-        u'INSERT DATA { %s }' % g.serialize(format='nt')
-    )
+sp.setQuery(queryString)
 
 
-    """read data from stardog"""
-
-    queryString = "SELECT * WHERE { ?s ?p \"阿胶汤2\". }"
-    sp = SPARQLWrapper("http://123.206.66.105:5820/myDB/query")
-
-    #sp.setHTTPAuth(DIGEST)
-    sp.setCredentials("admin", "admin")
-    sp.setMethod(POST)
-
-    # add a default graph, though that can also be part of the query string
-    #sp.addDefaultGraph("http://localhost:5820/myDB/graph-selected")
-
-    sp.setQuery(queryString)
-    sp.setReturnFormat("json")
-    ret = sp.query()
-    for v in ret:
-        print(v)
-
-
-
-    return HttpResponse("Hello, World!  You're at the polls index.")
