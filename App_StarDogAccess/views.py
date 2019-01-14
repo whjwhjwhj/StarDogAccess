19cce3b6151bcd54658d922212006e49276f660c
whjwhjwhj <weihjemail@gmail.com>
2019-01-14 18:17:52 +0800

    2018/12/21 同步

--- App_StarDogAccess/views.py
+++ App_StarDogAccess/views.py
@@ -1,19 +1,18 @@
-from SPARQLWrapper import SPARQLWrapper, POST, DIGEST
-from rdflib.plugins.stores import sparqlstore
-from rdflib import Graph, Literal, URIRef
+from SPARQLWrapper import SPARQLWrapper, BASIC
+from django.shortcuts import render
+from django.http import HttpRequest
 from django.http import HttpResponse
-import rdflib
+import json
 
-queryString = "INSERT DATA { <http://example/book1>  <http://purl.org/dc/elements/1.1/subject>  \"WeiHaiJunbook\". }"
-sp = SPARQLWrapper("http://123.206.66.105:5820/TestDB/query")
+def MeasurementData(request):
+    if request.method == 'POST':
+        print('weihaijun are in POST request')
+        json_results =  HttpRequest.POST
+        print(json_results)
+        # datas = json.load(json_results)
+        #
+        # for data in datas:
+        #     print(data)
 
-#sp.setHTTPAuth(DIGEST)
-sp.setCredentials("admin", "admin")
-sp.setMethod(POST)
-
-# add a default graph, though that can also be part of the query string
-#sp.addDefaultGraph("http://localhost:5820/myDB/graph-selected")
-
-sp.setQuery(queryString)
-
+        return HttpResponse(request)
 
--- StarDogAccess/settings.py
+++ StarDogAccess/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0lp^@3v+xl&1z7+6jzp1l_e^w642gfhjtv_=k5-)dcwd@f-yvp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.4.14']
 
 
 # Application definition
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
